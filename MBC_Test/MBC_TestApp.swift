a758f29a28aed5c9cffb9ae2b17637a48e819574
Ricardo Guerrero <Ricarguerrero02@gmail.com>
2023-11-09 11:05:08 -0600

    Initial Commit

---
+++ MBC_Test/MBC_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBC_TestApp.swift
+//  MBC_Test
+//
+//  Created by Ricardo Guerrero Godínez on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MBC_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
